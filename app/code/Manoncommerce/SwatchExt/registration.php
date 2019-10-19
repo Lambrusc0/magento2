eb993952dc9945ca92d18dfeb7afbec07c08d5b1
Gabor Juhasz <gaborjuhasz@Gabors-Mac-mini.local>
2019-10-19 12:00:32 +0100

    First commit

---
+++ app/code/Manoncommerce/SwatchExt/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Manoncommerce_SwatchExt',
+        __DIR__
+    );
